cbb4e136acf55a6ceefaef8b5bada5bf045e437c
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-10-30 16:28:04 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-res.send('Bienvenido');
+    res.send('Bienvenido');
 });
 
 module.exports = router;
