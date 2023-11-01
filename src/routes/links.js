945053e0fc59adfea2d737ee4fbdc639c6cce3cb
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-01 14:36:06 -0500

    Update links.js

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const pool= require('../database');
+
 router.get('/register',(req, res) =>{
 res.render('links/register');
 });
 
-router.post('/register',(req, res) =>{
-    res.render('datos recibidos');
-    });
-    
+router.post('/register', (req, res)=> {
+res.send('recibido')
+});
 module.exports= router;
