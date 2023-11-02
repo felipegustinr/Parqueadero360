0204769b321cfe446daa1b42e0b9536a52c1a397
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-01 23:55:29 -0500

    Create users.js

---
+++ src/routes/users.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const pool= require('../database');
+
+router.get('/register',(req, res) =>{
+res.render('users/register');
+});
+
+router.post('/register', async (req, res)=> {
+
+    const {nombres, email, phone, address } = req.body
+    const new_User = {
+        
+        nombres,
+        email,
+        phone,
+        address
+    };
+    await pool.query('INSERT INTO Usuario (nombres, email, phone, address) VALUES (?, ?, ?, ?)', [new_User.nombres, new_User.email, new_User.phone, new_User.address]);
+    res.send('Datos enviados')
+});
+router.get('/', async (req, res)=>{
+    const users= await pool.query('SELECT * FROM ParqueaderoTest.Usuario;');
+    console.log(users)
+    res.send('usuarios listados')
+});
+
+
+module.exports= router;
