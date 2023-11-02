9f17d1e3defc6e8299823eccd75d7246d7232a3f
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-01 22:35:32 -0500

    Update links.js

--- src/routes/links.js
+++ src/routes/links.js
@@ -9,16 +9,16 @@ res.render('links/register');
 
 router.post('/register', async (req, res)=> {
 
-    const {idUsuario = 1, nombres, email, phone, address } = req.body
+    const {nombres, email, phone, address } = req.body
     const new_User = {
-        idUsuario,
+        
         nombres,
         email,
         phone,
         address
     };
-    await pool.query('INSERT INTO usuario set ?', [new_User]);
-    console.log(new_User)
+    await pool.query('INSERT INTO Usuario VALUES (idUsuario,nombres,email,phone,address)',[new_User, new_User.nombres,new_User.email,new_User.phone,new_User.address]);
+    console.log(new_User);
     res.send('recibido');
 });
 module.exports= router;
