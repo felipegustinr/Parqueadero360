3f197aee9ebd7b7720b492d2b3fd418e04db2ea1
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-01 23:55:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 //rutas
 app.use(require('./routes/index'));
 app.use(require('./routes/authentication'));
-app.use('/links',require('./routes/links'));
+app.use('/users',require('./routes/users'));
 
 //archivos publicos
 app.use(express.static(path.join(__dirname,'public')));
