c6662ceba5974bfa9229023a605ef80d67f5898d
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-03 17:09:19 -0500

    Update handlebars.js

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -4,6 +4,6 @@ const {format, render} = require ('timeago.js');
 const helpers ={};
 
 helpers.timeago = (timestamp) =>{
-    return render,timestamp
+    return format(timestamp)
 }
 module.exports = helpers;
