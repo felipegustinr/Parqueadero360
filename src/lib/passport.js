7c4d9af61d397762c391f7de4fbb04ce10e75df7
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-06 14:53:41 -0500

    Update passport.js

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -1,12 +1,21 @@
-const passport = require ('passport');
-const LocalStrategy = require ('passport-local');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const pool = require('../database')
 
 passport.use('local.signup', new LocalStrategy({
-    usernameField : 'nombreUsuario',
-    passwordField : 'password',
-    passReqToCallback : true
-}, async (req, nombreUsuario, password, done)=> {
-    console.log(req.body);
+    usernameField: 'nombreUsuario',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, nombreUsuario, password, done) => {
+    const { nombresCompletos } = req.body;
+    const newUser = {
+        nombreUsuario,
+        password,
+        nombresCompletos
+
+    };
+    await pool.query('INSERT INTO Usuario (nombresCompletos, nombreUsuario, password) VALUES (?, ?, ?)', newUser.nombresCompletos, newUser.nombreUsuario, newUser.password);
 }));
 
 // passport.serializeUser((nombreUsuario, done)=> {
