530d74c014e30748db0766384f6a0b1d8342b0cf
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-06 13:59:19 -0500

    Update authentication.js

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport')
+
 router.get('/signup', (req, res) => {
     res.render('auth/signup');
 });
 
-router.post('/signup/',(req,res)=>{
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true,
+}));
 
+
+
+router.get('/profile', (req, res) => {
+    res.send('Perfil de usuario')
 });
-module.exports= router;
+
+module.exports = router;
