e07f800d8f819514b40d838ef1de9e0cad9adadd
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-05 23:02:49 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 });
 
 //rutas
-app.use(require('./routes/index'));
+app.use(require('./routes'));
 app.use(require('./routes/authentication'));
 app.use('/vehicles',require('./routes/vehicles'));
 
