cadef80a0417f710a84a2e77e56f6ac9bfb51baa
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-06 13:59:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
+const passport = require ('passport');
 
 const {database} = require('./keys');
 //inicializacion
 const app= express();
+require('./lib/passport');
 
 
 //configuracion
@@ -35,6 +37,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 //variables globales
