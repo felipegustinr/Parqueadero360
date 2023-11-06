6bddcdb1f1642770379ef2a4a86e62249dcb2ddb
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-06 00:46:28 -0500

    Update authentication.js

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/signup', (req, res) => {
-    
+    res.render('/auth/signup');
+});
+
+router.post('/signup',(req,res)=>{
+
 });
 module.exports= router;
