7026c47089bc44d7b583c209a3a2e1c00575d663
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-08 02:16:28 -0500

    Update helpers.js

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,16 +1,16 @@
 const bcrypt = require('bcryptjs');
 const helpers = {};
 helpers.encryptPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(12);
     const hash = await bcrypt.hash(password, salt);
     return hash;
 };
 
-helpers.matchPassword = async (password, savePassword) => {
+helpers.matchPassword = async (password, savedPassword) => {
     try {
-        return await bcrypt.compare(password, savePassword);
+        await bcrypt.compare(password, savedPassword);
     } catch (error) {
         console.log(error)
     }
-}
+};
 module.exports = helpers;
