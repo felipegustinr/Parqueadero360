9ee7a058858bc7e34fa8f58f4fb5fdc662333792
felipegustinr <79724739+felipegustinr@users.noreply.github.com>
2023-11-14 22:34:16 -0500

    Update vehicles.js

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
-
 const pool= require('../database');
+
+// MODULO DE REGISTRO DE VEHICULOS
 
 router.get('/register',(req, res) =>{
 res.render('vehicles/register');
@@ -23,11 +23,14 @@ router.post('/register', async (req, res)=> {
     res.redirect('/vehicles')
 });
 
+// MODULO DE LISTADO DE VEHICULOS
+
 router.get('/', async (req, res)=>{
     const vehicles= await pool.query('SELECT * FROM ParqueaderoTest.Vehiculo;');
     res.render('vehicles/list',{vehicles});
 });
 
+// MODULO DE ELIMINADO DE VEHICULOS
 router.get('/delete/:idVehiculo', async (req, res) =>{
     const {idVehiculo} = req.params;
     await pool.query('DELETE FROM Vehiculo WHERE (idVehiculo = ? )',[idVehiculo]);
@@ -35,6 +38,7 @@ router.get('/delete/:idVehiculo', async (req, res) =>{
     res.redirect('/vehicles');
 });
 
+// MODULO EDICIÓN DE VEHICULOS
 router.get('/edit/:idVehiculo', async (req,res)=>{
     const{idVehiculo} = req.params;
     const vehicles = await pool.query('SELECT * FROM Vehiculo WHERE (idVehiculo = ?)',[idVehiculo]);
